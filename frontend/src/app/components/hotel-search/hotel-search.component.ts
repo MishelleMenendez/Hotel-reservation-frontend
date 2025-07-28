b18e93faa4ab4408a875bb559be9e222bf91e20f
Ismoor <mperaltam4@est.ups.edu.ec>
2025-07-28 01:40:55 -0500

    Hotel Search

--- frontend/src/app/components/hotel-search/hotel-search.component.ts
+++ frontend/src/app/components/hotel-search/hotel-search.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { HotelService } from '../../services/hotel.service';
 
 @Component({
   selector: 'app-hotel-search',
-  standalone: false,
   templateUrl: './hotel-search.component.html',
-  styleUrl: './hotel-search.component.css'
+  styleUrls: ['./hotel-search.component.css']
 })
 export class HotelSearchComponent {
+  location = '';
+  startDate = '';
+  endDate = '';
+  hotels: any[] = [];
+  searched = false;
 
+  constructor(private hotelService: HotelService) {}
+
+  searchHotels() {
+    this.hotelService.searchHotels(this.location, this.startDate, this.endDate).subscribe({
+      next: (data) => {
+        this.hotels = data;
+        this.searched = true;
+      },
+      error: () => {
+        this.hotels = [];
+        this.searched = true;
+      }
+    });
+  }
+
+  selectHotel(hotel: any) {
+    // Puedes emitir evento o navegar a detalle
+  }
 }
