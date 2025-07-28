a6f27862ff26d97aff6623900a0a3832fffe180a
MishelleMenendez <bmenendezj@est.ups.edu.ec>
2025-07-28 01:44:16 -0500

    Hotel Detail

--- frontend/src/app/components/hotel-detail/hotel-detail.component.ts
+++ frontend/src/app/components/hotel-detail/hotel-detail.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { HotelService } from '../../services/hotel.service';
 
 @Component({
   selector: 'app-hotel-detail',
-  standalone: false,
   templateUrl: './hotel-detail.component.html',
-  styleUrl: './hotel-detail.component.css'
+  styleUrls: ['./hotel-detail.component.css']
 })
-export class HotelDetailComponent {
+export class HotelDetailComponent implements OnChanges {
+  @Input() hotelId: number | null = null;
+  hotel: any = null;
 
+  constructor(private hotelService: HotelService) {}
+
+  ngOnChanges() {
+    if (this.hotelId) {
+      this.hotelService.getHotelDetails(this.hotelId).subscribe({
+        next: (data) => this.hotel = data,
+        error: () => this.hotel = null
+      });
+    }
+  }
 }
