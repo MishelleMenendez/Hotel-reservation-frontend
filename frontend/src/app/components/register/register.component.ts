edafb1a82d13cc698c042b362494cde8edba9296
Ismoor <mperaltam4@est.ups.edu.ec>
2025-07-28 01:34:06 -0500

    Register

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  standalone: false,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  name = '';
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.errorMessage = '';
+    this.authService.register(this.name, this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: () => this.errorMessage = 'Error en el registro, intente con otro email.'
+    });
+  }
 }
