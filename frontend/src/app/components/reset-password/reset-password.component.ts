bf07ec0a5007597376ef1b3f0ddddf838f3f2603
Ismoor <mperaltam4@est.ups.edu.ec>
2025-07-28 01:36:37 -0500

    Reset password

--- frontend/src/app/components/reset-password/reset-password.component.ts
+++ frontend/src/app/components/reset-password/reset-password.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-reset-password',
-  standalone: false,
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.css'
+  styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+  email = '';
+  newPassword = '';
+  message = '';
+  error = false;
 
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.message = '';
+    this.error = false;
+    this.authService.resetPassword(this.email, this.newPassword).subscribe({
+      next: () => {
+        this.message = 'Contraseña restablecida correctamente.';
+      },
+      error: () => {
+        this.error = true;
+        this.message = 'Error al restablecer contraseña.';
+      }
+    });
+  }
 }
