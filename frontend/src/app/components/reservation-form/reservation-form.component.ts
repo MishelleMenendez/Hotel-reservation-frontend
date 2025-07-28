57b10548f87ec023f828403a1ffe3e2c691ea915
MishelleMenendez <bmenendezj@est.ups.edu.ec>
2025-07-28 01:52:05 -0500

    Reservation form

--- frontend/src/app/components/reservation-form/reservation-form.component.ts
+++ frontend/src/app/components/reservation-form/reservation-form.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HotelService } from 'src/app/services/hotel.service';
+import { ReservationService } from 'src/app/services/reservation.service';
 
 @Component({
   selector: 'app-reservation-form',
-  standalone: false,
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+  reservationForm!: FormGroup;
+  hotels: any[] = [];
+  rooms: any[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private hotelService: HotelService,
+    private reservationService: ReservationService
+  ) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.fb.group({
+      check_in: ['', Validators.required],
+      check_out: ['', Validators.required],
+      hotel_id: ['', Validators.required],
+      room_id: ['', Validators.required]
+    });
+
+    this.hotelService.getHotels().subscribe(data => {
+      this.hotels = data;
+    });
+  }
+
+  onHotelChange(hotelId: string): void {
+    this.hotelService.getHotelRooms(hotelId).subscribe(data => {
+      this.rooms = data;
+    });
+  }
+
+  submitReservation(): void {
+    if (this.reservationForm.valid) {
+      this.reservationService.createReservation(this.reservationForm.value).subscribe();
+    }
+  }
 }
