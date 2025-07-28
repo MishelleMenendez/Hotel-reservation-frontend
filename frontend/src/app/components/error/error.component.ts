ae83567fc9eb196d58d819dc551c1d285c4320e7
Ismoor <mperaltam4@est.ups.edu.ec>
2025-07-28 01:55:18 -0500

    Error component

--- frontend/src/app/components/error/error.component.ts
+++ frontend/src/app/components/error/error.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  standalone: false,
   templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+  styleUrls: ['./error.component.css']
 })
-export class ErrorComponent {
-
-}
+export class ErrorComponent {}
